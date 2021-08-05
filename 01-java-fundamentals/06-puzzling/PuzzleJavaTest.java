0c4a2574853ee0987ae6870c476544ecdc85a7ee
reena <reena.dangi@gmail.com>
2021-08-05 00:06:20 -0500

    Puzzling updated

---
+++ 01-java-fundamentals/06-puzzling/PuzzleJavaTest.java
@@ -0,0 +1,7 @@
+public class PuzzleJavaTest {
+    public static void main(String[] args) {
+        PuzzleJava myPuzzle=new PuzzleJava();
+        System.out.println(myPuzzle.getTenRolls());
+
+    }
+}
